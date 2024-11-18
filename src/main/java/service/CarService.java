344be09c7dddd1b9ee182badea2924ca140ea685
pumnea <apumnea.dev@gmail.com>
2024-11-19 00:38:17 +0200

    (add) service layer for the business logic

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,85 @@
+package service;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import model.Car;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import repository.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * @author Alex Pumnea
+ */
+public class CarService {
+    private static final Logger logger = LoggerFactory.getLogger(CarService.class);
+
+    private final Validator validator;
+    private final Repository<Car> repository;
+    private final Fetcher scraper;
+
+    public CarService(Repository<Car> repository, Fetcher scraper) {
+        this.repository = repository;
+        this.scraper = scraper;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        this.validator = factory.getValidator();
+
+    }
+
+    public void addFetchedCars(String url) {
+        List<Car> cars = parseCars(url);
+        repository.addAll(cars);
+    }
+
+    private List<Car> parseCars(String url) {
+        List<Car> cars = new ArrayList<>();
+        Elements carElements = scraper.getCarElements(url);
+
+        for (Element carElement : carElements) {
+            String makeAndYear = carElement.getElementsByClass("js-item-ad").text();
+
+            if (validData(makeAndYear) || isBooster(carElement)) continue;
+
+            String[] parts = makeAndYear.split(", ");
+            String make = parts[0].trim();
+            String year = parts.length > 1 ? parts[1].replace(" г.", "").trim() : scraper.getYearFromDetails(carElement);
+            String price = carElement.getElementsByClass("ads-list-photo-item-price-wrapper")
+                    .text()
+                    .replace(" €", "")
+                    .replace("$", "")
+                    .replaceAll("\\s", "");
+            Car car = new Car(make, year, price);
+            validateCar(car);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    private static boolean isBooster(Element carElement) {
+        return carElement.hasClass("js-booster-inline");
+    }
+
+    private static boolean validData(String makeAndYear) {
+        return makeAndYear.isEmpty();
+    }
+
+    private void validateCar(Car car) {
+        Set<ConstraintViolation<Car>> violations = validator.validate(car);
+        if (!violations.isEmpty()) {
+            violations.forEach(v -> logger.warn("Validation failed for car '{}': {}", car, v.getMessage()));
+        }
+    }
+
+    public List<Car> getAll(String url) {
+        addFetchedCars(url);
+        return repository.getAll();
+    }
+}
